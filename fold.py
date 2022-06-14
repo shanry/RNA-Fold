0f1b68def9b39e56c7cd179046d97854a87531e6
Tianshuo Zhou <zhoutian@pelican02.eecs.oregonstate.edu>
2022-06-13 21:48:41 -0700

    algo 2

--- fold.py
+++ fold.py
@@ -1,8 +1,8 @@
 import argparse
 
-from k_best import algo_0
+from k_best import algo_0, algo_1, algo_2
 
-def test(k):
+def test(algo, k):
     rnas = [
         "ACAGU",
         "AC",
@@ -19,7 +19,7 @@ def test(k):
     ]
     print()
     for rna in rnas:
-        best_1, num_struct, best_k = algo_0(rna, k)
+        best_1, num_struct, best_k = algo(rna, k)
         print(rna)
         print(best_1)
         print(num_struct)
@@ -31,10 +31,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--rna", type=str, default="GCACG")
     parser.add_argument("--k", type=int, default=10)
+    parser.add_argument("--algo", type=int, default=0)
     parser.add_argument("--test", action='store_true')
     args = parser.parse_args()
     print(f"args: {args}")
-    best_1, num_struct, best_k = algo_0(args.rna, args.k)
+    algo = algo_0
+    if args.algo == 1:
+        algo = algo_1
+    if args.algo == 2:
+        algo = algo_2
+    best_1, num_struct, best_k = algo(args.rna, args.k)
     print()
     print(args.rna)
     print(best_1)
@@ -42,5 +48,5 @@ if __name__ == "__main__":
     print(best_k)
     
     if args.test:
-        test(args.k)
+        test(algo, args.k)
     
