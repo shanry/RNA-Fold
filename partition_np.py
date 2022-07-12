93f6fb535d7d560733b76dd2ac3f9cc85bb39545
Tianshuo Zhou <zhoutian@mozart.eecs.oregonstate.edu>
2022-07-12 11:23:33 -0700

    numpy array as dictionary

--- partition_np.py
+++ partition_np.py
@@ -7,145 +7,113 @@ from k_best import algo_all
 np.set_printoptions(linewidth=200)
 
 pairs = {'au', 'gc', 'gu'}
+sharpturn = 0
 
 def match(x, y):
     return (x+y).lower() in pairs or (y+x).lower() in pairs
 
 
-def count_inside(s): # backward inside
+def set_sharpturn(sharpturn_val):
+    global sharpturn
+    sharpturn = sharpturn_val
+    print(f"sharpturn is: {sharpturn}")
+
+
+def inside_backward(s): # backward inside
     assert len(s) > 1, "the length of rna should be at least 2!"
     n = len(s)
-    counts = np.ones((n, n), dtype=int) # number of structures
+    counts = np.zeros((n, n+1), dtype=int) # number of structures
+    for k in range(n):
+        counts[k][k]=1
+        counts[k][k-1] = 1
     for l in range(1, n):
         for i in range(0, n-l):
             j = i + l
-            
-            # Case 1 doesn't exist here because l starts from 1
-            # Case 2
-            counts[i, j] = counts[i, j-1]
-            
-            # Case 3
+            counts[i, j] += counts[i, j-1] # x.
             for t in range(i, j):
-                if match(s[t],  s[j]):
-                        
-                    # deal with number of structures
-                    count_left = counts[i, t-1] if i<(t-1) else 1
-                    count_right = counts[t+1, j-1] if (t+1)<(j-1) else 1
-                    counts[i, j] += count_left*count_right
-                    
+                if match(s[t],  s[j]) and j-t>sharpturn: # (x); x(x)    
+                    count_left = counts[i, t-1] 
+                    count_right = counts[t+1, j-1] 
+                    counts[i, j] += count_left*count_right     
     return counts
 
 
-def count_inside_2(s):
+def inside_forward(s):
     assert len(s) > 1, "the length of rna should be at least 2!"
     n = len(s)
-    counts = np.ones((n, n), dtype=int) # number of structures
+    counts = np.ones((n, n+1), dtype=int) # number of structures
+    for k in range(n):
+        counts[k][k]=1
+        counts[k][k-1] = 1
     for j in range(1, n):
         for i in range(0, j):
-            counts[i, j] = counts[i, j-1]
-            if match(s[i], s[j]):
-                counts_right = counts[i+1, j-1] if i+1<=j-1 else 1
+            counts[i, j] += counts[i, j-1] # x.
+            if match(s[i], s[j]) and j-i>sharpturn: # (x); x(x) 
+                counts_right = counts[i+1, j-1] 
                 for t in range(0, i):
-                    counts_left = counts[t, i-1] if t<=i-1 else 1
+                    counts_left = counts[t, i-1] 
                     counts[t, j] += counts_left*counts_right
                 counts[i, j] += counts_right
     return counts
 
 
-def count_outside(s, counts=None): 
+# def outside_backward_(s, counts):  # algorithm in the supplementary of linear partition
+#     assert len(s) > 1, "the length of rna should be at least 2!"
+#     assert len(s) == len(counts), "the length of rna should match counts matrix!"
+#     n = len(s)
+#     counts_out = np.zeros((n, n+1), dtype=int) # number of structures, n-th column indexes the position -1
+#     # p = defaultdict(lambda: defaultdict(int)) # pairing
+#     # counts_out = np.zeros((n, n), dtype=np.uint) # # number of structures
+#     p = np.zeros((n, n), dtype=int) # pairing
+#     counts_out[0][n-1] = 1
+#     for j in range(n-1, -1, -1):
+#         for i in range(j, -1, -1 ): # end with j-1, start with i
+#             counts_out[i][j-1] += counts_out[i][j] # x.
+#             if i>0 and match(s[i-1], s[j]) and j-(i-1)>sharpturn: # (x); x(x) 
+#                 counts_right = counts[i, j-1] #if i<=j-1 else 1
+#                 for t in range(i-1, -1, -1): # end with i-2, start with t
+#                     counts_left = counts[t, i-2] #if t<=i-2 else 1
+#                     counts_outleft = counts_out[t][j]
+#                     counts_out[t][i-2] += counts_outleft*counts_right  # pop left
+#                     counts_out[i][j-1] += counts_outleft*counts_left # pop right
+#                     p[i-1][j] += counts_outleft*counts_left*counts_right # *counts[i, j-1] # count pairs
+#     for i in range(len(p)):
+#         for j in range(len(p[i])):
+#             if p[i][j] > 0:
+#                 print(i+1, j+1, p[i][j])
+#     return counts_out, p
+
+
+def outside_backward(s, inside):  # algorithm in the supplementary of linear partition
     assert len(s) > 1, "the length of rna should be at least 2!"
-    if counts is None:
-        counts = count_inside(s)
-    assert len(s) == len(counts) and len(s)==len(counts.T), "the length of rna should match counts matrix!"
+    assert len(s) == len(inside), "the length of rna should match counts matrix!"
     n = len(s)
-    counts_out = np.zeros((n, n), dtype=int) # number of structures
-    counts_out[0, -1] = 1
-    for l in range(n-2, -1, -1):
-        for i in range(0, n-l):
-            j = i + l
-            # print(f"l: {l}, i: {i}, j: {j}")
-            if j==n-1:
-                counts_out[i, j] = counts[0, i-1]
-            elif i==0:
-                counts_out[i, j] = counts[j+1, n-1]
-            else:
-                # j+1 unpaired
-                counts_out[i, j] = counts_out[i, j+1]
-                # print(f'unpaired: {counts[i, j+1]}')
-                # j+1 paired rightward
-                for t in range(j+2, n):
-                    if match(s[j+1],  s[t]):
-                        counts_out[i, j] += counts_out[i, t]*counts[j+2, t-1]
-                        # print(f'rightward: {counts_out[i, t]*counts[j+2, t-1]}')
-                # j+1 paired leftward
-                for t in range(0, i):
-                    if match(s[t],  s[j+1]):
-                        counts_out[i, j] += counts_out[t, j+1]*counts[t+1, i-1]
-                        # print(f'leftward: {counts_out[t, j+1]*counts[t+1, i-1]}')
-    return counts_out
-
-
-def count_outside_2(s, counts=None):
-    assert len(s) > 1, "the length of rna should be at least 2!"
-    if counts is None:
-        counts = count_inside(s)
-    assert len(s) == len(counts) and len(s)==len(counts.T), "the length of rna should match counts matrix!"
-    n = len(s)
-    counts_out = np.zeros((n, n), dtype=int) # number of structures
-    counts_out[0, -1] = 1
-    for i in range(1, n):
-        counts_out[i, n-1] = counts[0, i-1]
-    for j in range(n-1, 0, -1):
-        for i in range(0, j):
-            counts_out[i, j-1] += counts_out[i, j]
-            if match(s[i], s[j]):
-                counts_right = counts[i+1, j-1] if i+1<=j-1 else 1
-                for t in range(0, i):
-                    counts_outleft = counts_out[t, j]
-                    counts_out[t, i-1] += counts_outleft*counts_right  # pop left
-                counts_outright = counts_out[i, j]
-                for t in range(i+2, j):
-                    counts_left = counts[i+1, t-1]
-                    counts_out[t, j-1] += counts_outright*counts_left # pop right
-                if i+1<=j-1:
-                    counts_out[i+1, j-1] += counts_outright
-    return counts_out
-
-
-def count_outside_backward(s, counts=None):  # algorithm in the supplementary of linear partition
-    assert len(s) > 1, "the length of rna should be at least 2!"
-    if counts is None:
-        counts = count_inside(s)
-    assert len(s) == len(counts) and len(s)==len(counts.T), "the length of rna should match counts matrix!"
-    n = len(s)
-    counts_out = np.zeros((n, n+1), dtype=int) # number of structures, n-th column indexes the position -1
+    outside = np.zeros((n, n+1), dtype=int) # number of structures, n-th column indexes the position -1
     # p = defaultdict(lambda: defaultdict(int)) # pairing
     # counts_out = np.zeros((n, n), dtype=np.uint) # # number of structures
     p = np.zeros((n, n), dtype=int) # pairing
-    counts_out[0][n-1] = 1
+    outside[0][n-1] = 1
     for j in range(n-1, -1, -1):
         for i in range(j, -1, -1 ): # end with j-1, start with i
-            counts_out[i][j-1] += counts_out[i][j]
-            if i>0 and match(s[i-1], s[j]):
-                counts_right = counts[i, j-1] if i<=j-1 else 1
+            outside[i][j-1] += outside[i][j] # x.
+            if i>0 and match(s[i-1], s[j]) and j-(i-1)>sharpturn: # (x); x(x) 
+                counts_right = inside[i, j-1] #if i<=j-1 else 1
                 for t in range(i-1, -1, -1): # end with i-2, start with t
-                    counts_left = counts[t, i-2] if t<=i-2 else 1
-                    counts_outleft = counts_out[t][j]
-                    counts_out[t][i-2] += counts_outleft*counts_right  # pop left
-                    counts_out[i][j-1] += counts_outleft*counts_left # pop right
-                    p[i-1][j] += counts_outleft*counts_left*counts_right # *counts[i, j-1] # count pairs
+                    counts_left = inside[t, i-2] #if t<=i-2 else 1
+                    counts_out = outside[t][j]
+                    outside[t][i-2] += counts_out*counts_right  # pop left
+                    outside[i][j-1] += counts_out*counts_left # pop right
+                    p[i-1][j] += counts_out*counts_left*counts_right # *counts[i, j-1] # count pairs
     for i in range(len(p)):
         for j in range(len(p[i])):
             if p[i][j] > 0:
                 print(i+1, j+1, p[i][j])
-    return counts_out, p
+    return outside, p
 
 
-def count_outside_forward(s, inside=None):
+def outside_forward(s, inside):
     assert len(s) > 1, "the length of rna should be at least 2!"
-    if inside is None:
-        inside = count_inside(s)
-    assert len(s) == len(inside) and len(s)==len(inside.T), "the length of rna should match counts matrix!"
+    assert len(s) == len(inside), "the length of rna should match counts matrix!"
     n = len(s)
     # outside = defaultdict(lambda: defaultdict(int))
     # p = defaultdict(lambda: defaultdict(int))
@@ -155,63 +123,27 @@ def count_outside_forward(s, inside=None):
     for l in range(n, 0, -1):
         for j in range(n-1, l-2, -1): # end: from n-1 to l-1
             i = j-l+1  # start: j-(l-1)
-            outside[i][j-1] += outside[i][j] # unpaired j
+            outside[i][j-1] += outside[i][j] # # x.
             for k in range(j-1, i-1, -1): # split: for j-1 to i
-                # print(l, j, i, k)
-                if match(s[k], s[j]):
-                    # print(l, j, i, k)
-                    right = inside[k+1][j-1] if k+1<=j-1 else 1
-                    left = inside[i][k-1] if i<=k-1 else 1
+                if match(s[k], s[j]) and j-k>sharpturn: # (x); x(x) 
+                    right = inside[k+1][j-1] #if k+1<=j-1 else 1
+                    left = inside[i][k-1] # if i<=k-1 else 1
                     outside[i][k-1] += outside[i][j]*right # pop left
                     outside[k+1][j-1] += outside[i][j]*left # pop right
                     p[k][j] += outside[i][j]*left*right
-    # for i in p:
-    #     for j in p[i]:
-    #         if p[i][j] > 0:
-    #             print(i+1, j+1, p[i][j])
     for i in range(len(p)):
         for j in range(len(p[i])):
             if p[i][j] > 0:
                 print(i+1, j+1, p[i][j])
     return outside, p
+
+
+def inside_outside(algo_in, algo_out, rna):
+    inside = algo_in(rna)
+    outside, pair = algo_out(rna, inside)
+    return inside, outside, pair
+
                     
-
-def pair_match(ss):
-    index_pairs = []
-    stack = []
-    for i, c in enumerate(ss):
-        if c == "(":
-            stack.append(i)
-        elif c == ")":
-            j = stack.pop()
-            index_pairs.append((j, i))
-        else:
-            index_pairs.append((i, i))
-    return index_pairs
-
-def counts_per_pair(ss_list):
-    assert len(ss_list) > 0
-    n = len(ss_list[0])
-    counts_pp = np.zeros((n, n), dtype=int)
-    for i, ss in enumerate(ss_list):
-        index_pairs = pair_match(ss)
-        for pair in index_pairs:
-            counts_pp[pair[0], pair[1]] += 1
-    return counts_pp
-
-def verify(p_pair, p_in, p_out):
-    n = len(p_pair)
-    for i in range(n):
-        for j in range(i, n):
-            if j==i:
-                assert p_pair[i, i] == p_out[i, i] # \beta(i,i) = # of derivations that contain unpaired i
-            elif p_pair[i, j] > 0:
-                if j==i+1:
-                    assert p_pair[i, j] == p_out[i, j] # \beta(i,j) = # of derivations that contain (i,j)
-                else:
-                    assert p_pair[i, j] == p_in[i+1, j-1]*p_out[i, j] # \alpha(i+1,j-1) * \beta(i,j) = # of derivations that contain (i,j)
-
-
 def test(algo_in, algo_out):
     rnas = [
         "ACAGU",
@@ -229,146 +161,30 @@ def test(algo_in, algo_out):
     ]
     print()
     for i, rna in enumerate(rnas):
-        test_counts_in_out(algo_in, algo_out, rna)
+        inside_outside(algo_in, algo_out, rna)
+
         
-
-def test_counts_in_out(algo_in, algo_out, rna):
-    p_in = algo_in(rna)
-    print('rna:', rna)
-    print('count inside:')
-    print(p_in)
-    p_out = algo_out(rna, p_in)
-    print('count outside:')
-    print(p_out)
-
-    best_1, num_struct, best_all = algo_all(rna, None)
-    print('best:')
-    print(best_1)
-    pairs = pair_match(best_1[-1])
-    print('mathed pairs:')
-    print(pairs)
-
-    counts_pp = counts_per_pair([item[-1] for item in best_all])
-    print('count per pair:')
-    print(counts_pp)
-
-    verify(counts_pp, p_in, p_out)
-    print("verified!")
-    
-    
-def compare_counts(rna):
-    p_in = count_inside(rna)
-    print('rna:', rna)
-    print('count inside:')
-    print(p_in)
-    
-    p_in_2 = count_inside_2(rna)
-    print('count inside 2:')
-    print(p_in_2)
-    assert np.array_equal(p_in, p_in_2)
-    
-    p_out = count_outside(rna, p_in)
-    print('count outside:')
-    print(p_out)
-    
-    p_out_2 = count_outside_2(rna, p_in_2)
-    print('count outside 2:')
-    print(p_out_2)
-    assert np.array_equal(p_out, p_out_2)
-    
-    print("verified!")
-    
-    
-def compare_counts_supp(rna):
-    p_in = count_inside(rna)
-    print('rna:', rna)
-    print('count inside:')
-    print(p_in)
-    
-    p_in_2 = count_inside_2(rna)
-    print('count inside 2:')
-    print(p_in_2)
-    assert np.array_equal(p_in, p_in_2)
-    
-    p_out = count_outside(rna, p_in)
-    print('count outside:')
-    print(p_out)
-    
-    p_out_supp = count_outside_supp(rna, p_in_2)
-    print('count outside supp:')
-    print(p_out_supp)
-    assert np.array_equal(p_out, p_out_supp)
-    
-    print("verified!")
-    
-
-def count(algo_in, algo_out, rna):
-    p_in = algo_in(rna)
-    print('rna:', rna)
-    print('count inside:')
-    print(p_in)
-    p_out = algo_out(rna, p_in)
-    print('count outside:')
-    print(p_out)
-    return p_in, p_out
-
-
-def inside_outside(algo_in, algo_out, rna):
-    n = len(rna)
-    p_in = algo_in(rna)
-    p_out = algo_out(rna, p_in)
-    print('count per pair:')
-    for i in range(n):
-        for j in range(i+1, n):
-            if match(rna[i], rna[j]) > 0:
-                if j==i+1:
-                    count_pair = p_out[i, j] # \beta(i,j) = # of derivations that contain (i,j)
-                else:
-                    count_pair = p_in[i+1, j-1]*p_out[i, j] # \alpha(i+1,j-1) * \beta(i,j) = # of derivations that contain (i,j)
-                print(f"{i+1} {j+1} {count_pair}")
-    print('count per unpaired:')
-    for i in range(n):
-        count_unpair = p_out[i, i]
-        print(f"{i+1} {count_unpair}")
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--rna", type=str, default="GCACG")
-    # parser.add_argument("--k", type=int, default=10)
-    parser.add_argument("--algo", type=int, default=1)
+    parser.add_argument("--sharpturn", type=int, default=0)
+    parser.add_argument("--inside",  type=str, default='forward')
+    parser.add_argument("--outside", type=str, default='backward')
     parser.add_argument("--test", action='store_true')
-    parser.add_argument("--count", action='store_true')
-    parser.add_argument("--inout", action='store_true')
-    parser.add_argument("--verify", action='store_true')
-    parser.add_argument("--forward", action='store_true')
-    parser.add_argument("--backward", action='store_true')
-    parser.add_argument("--compare", action='store_true')
     args = parser.parse_args()
     print('args:')
     print(args)
-    if args.algo == 1:
-            partition_in = count_inside
-            partition_out = count_outside
+    set_sharpturn(args.sharpturn)
+    if args.inside == "forward":
+        partition_in = inside_forward
     else:
-        partition_in = count_inside_2
-        partition_out = count_outside_2
-    if args.count:
-        count(partition_in, partition_out, args.rna)
-    if args.inout:
-        inside_outside(partition_in, partition_out, args.rna)
-    if args.verify:
-        test_counts_in_out(partition_in, partition_out, args.rna)
-    if args.compare:
-        compare_counts(args.rna)
-        compare_counts_supp(args.rna)
-    if args.forward:
-        partition_out = count_outside_forward
-        p_in = partition_in(args.rna)
-        p_out = partition_out(args.rna, p_in)
-    if args.backward:
-        partition_out = count_outside_backward
-        p_in = partition_in(args.rna)
-        p_in = partition_out(args.rna, p_in)
+        partition_in = inside_backward
+    if args.outside == "forward":
+        partition_out = outside_forward
+    else:
+        partition_out = outside_backward
+        
+    inside_outside(partition_in, partition_out, args.rna)
+    
     if args.test:
         test(partition_in, partition_out)
